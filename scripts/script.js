81c24018bfa2e73f8e361d0f252fad9e6d0750cf
AndreyPopov <andreypopu@gmail.com>
2024-09-26 17:34:12 +0200

    Portfolio Rework

--- scripts/script.js
+++ scripts/script.js
@@ -5,63 +5,6 @@ var currentIcon;
 var index = 0;
 var currentVideo;
 
-function ShowNext(x, y, z)
-{
-    var imageSwitch = document.getElementById(y);
-    
-    if (z == '-') index--;
-    else index++;
-
-    if (index > 2) index = 0;
-    if (index < 0) index = 2;
-    
-    if (x == 'Sticky')
-    {
-        switch(index)
-        {
-            case 0: imageSwitch.src = "images/Sticky1.png"; break;
-            case 1: imageSwitch.src = "images/Sticky2.png"; break;
-            case 2: imageSwitch.src = "images/Sticky3.png"; break;
-        }
-    }
-    else if (x == 'Knighty')
-    {
-        switch(index)
-        {
-            case 0: imageSwitch.src = "images/Knighty1.png"; break;
-            case 1: imageSwitch.src = "images/Knighty2.png"; break;
-            case 2: imageSwitch.src = "images/Knighty3.png"; break;
-        }
-    }
-    else if (x == 'Roguelike')
-    {
-        switch(index)
-        {
-            case 0: imageSwitch.src = "images/Roguelike1.png"; break;
-            case 1: imageSwitch.src = "images/Roguelike2.png"; break;
-            case 2: imageSwitch.src = "images/Roguelike3.png"; break;
-        }
-    }
-    else if (x == 'Roll')
-    {
-        switch(index)
-        {
-            case 0: imageSwitch.src = "images/Roll1.png"; break;
-            case 1: imageSwitch.src = "images/Roll2.png"; break;
-            case 2: imageSwitch.src = "images/Roll3.png"; break;
-        }
-    }
-    else if (x == 'Blocky')
-    {
-        switch(index)
-        {
-            case 0: imageSwitch.src = "images/Blocky1.png"; break;
-            case 1: imageSwitch.src = "images/Blocky2.png"; break;
-            case 2: imageSwitch.src = "images/Blocky3.png"; break;
-        }
-    }
-}
-
 function ShowDiv(y) {
     var x = document.getElementById(y);
 
